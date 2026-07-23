e8123be799442415959f71f99c6989c69029874b
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-23 04:45:26 +0100

    Added-Next-Web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Appbar from "@/components/ui/Appbar";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-zinc-900">
+        <Appbar/>
+    </div>
+  );
+}
---
+++ web/src/components/ui/Appbar.tsx
@@ -0,0 +1,7 @@
+export default function Appbar(){
+    return(
+        <div className="w-full h-[10%] bg-black">
+            
+        </div>
+    )
+}
