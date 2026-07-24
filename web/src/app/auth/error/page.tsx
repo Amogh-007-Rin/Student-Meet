5dfbfbb03e214494ef8d6ae1d413cc9d47b36cd7
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-24 07:32:19 +0100

    Add auth error page

---
+++ web/src/app/auth/error/page.tsx
@@ -0,0 +1,7 @@
+export default function errorPage(){
+    return(
+        <div className="w-scren h-screen bg-zinc-600 flex justify-center items-center">
+                error page
+        </div>
+    );
+}
